b657a948d576ed944f583bc0371fb194b05ece7e
Rizo <jesus.rizo@iteso.mx>
2024-08-16 08:23:12 -0600

    Ejercicio 1

---
+++ Clase 1.c
@@ -0,0 +1,12 @@
+// Char es 1 byte, Int es 4 bytes y apuntadores son 8 bytes. 
+//Apuntadores son 8 bytes ya que se necesita suficiente espacio para guardar la dirección de memoria mas grande que se pueda referenciar.
+//Glosario
+// & para usar la direccion que hay y * es para cuando tu variable quieres "llevame al valor de la variable apuntada"
+
+#include <stdio.h>
+
+int main()
+{
+
+
+}
